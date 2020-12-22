982364bb4f9519e08348b0b206d1dd00ca197a3c
michael kremer <kremeremichael@gmail.com>
2020-12-22 13:35:09 -0700

    auto push run at Tue Dec 22 13:35:09 MST 2020

--- python/generate_manpage.py
+++ python/generate_manpage.py
@@ -8,6 +8,7 @@ import os
 import sys
 import argparse
 import subprocess
+import datetime
 # Defines
 # NONE
 
@@ -15,8 +16,11 @@ import subprocess
 ################################################
 # SET UP ARGUMENTS
 ################################################
-parser = argparse.ArgumentParser(description='A function build to recursively push nested git repos, child->parent')
+parser = argparse.ArgumentParser(description='A function build to auto-generate manpages for given functions', add_help=False)
 parser.add_argument('-f', '--file', help='File used to create manpage')
+parser.add_argument('-v', '--verbose', help='enable more verbose printing')
+parser.add_argument('-V', '--very-verbose', help='verbose + print lines as written to file')
+parser.add_argument('-h', '--help', help='help')
 args = parser.parse_args()
 
 
@@ -25,18 +29,69 @@ args = parser.parse_args()
 ################################################
 # get file
 if args.file:
-    print("wjat")
+    function_name = args.file.split('.')[0]
+    extension = args.file.split('.')[1]
     _file = open(args.file, 'r')
 else:
     print("ERROR: No file specified")
-    print("TODO: print argparse")
+    parser.print_help()
     sys.exit(1)
 
 # read lines
 lines = _file.readlines()
 for line in lines:
-    print(line)
-    
+    if extension == 'py':
+        pass
+    elif extension == '.sh':
+        pass
 
-# interpret my standard header
-# interpret argparse
+# Open file
+manpage = open(function_name + ".1", "w")
+
+# Write preamble
+manpage.write('.\\"\n')
+manpage.write('.\\" Man page for {}\n'.format(function_name))
+manpage.write('.\\"\n')
+manpage.write('.\\" {} {}\n'.format(os.getenv('FIRST_NAME'),os.getenv('LAST_NAME')))
+manpage.write('.\\" {}\n'.format(os.getenv('EMAIL')))
+manpage.write('.\\" Electrical Engineer\n')
+manpage.write('.\\" {}\n'.format(os.getenv('EMPLOYMENT')))
+manpage.write('.\\"\n')
+manpage.write('.\\" Automatically generatedy by generate_manpage.py\n')
+
+# Write lead line
+current_date = datetime.datetime.today()
+month = current_date.strftime('%B')
+manpage.write('.TH {} "{} {}, {}"\n'.format(function_name, month, current_date.day, current_date.year))
+manpage.write('.LO 1\n')
+
+# Write name
+manpage.write('.SH NAME\n')
+manpage.write('{} \\- TODO put short description here\n'.format(function_name))
+
+# Write synopsis
+manpage.write('.SH SYNOPSIS\n')
+manpage.write('.B {}\n'.format(function_name))
+#TODO arguments here
+manpage.write('')
+
+# Write description
+manpage.write('.SH DESCRIPTION\n')
+manpage.write('.B {}\n'.format(function_name))
+#TODO figure out description
+manpage.write('')
+
+# Write options
+manpage.write('.SH OPTIONS\n')
+for option in options:
+    manpage.write('.TP\n')
+    #TODO write argument flag
+    #TODO write description of argument
+manpage.write('')
+
+# Write author
+manpage.write(".SH  AUTHOR\n")
+manpage.write("{} {} <{}>.".format(os.getenv('FIRST_NAME'), os.getenv('LAST_NAME'), os.getenv('EMAIL')))
+
+#Done
+manpage.close()
